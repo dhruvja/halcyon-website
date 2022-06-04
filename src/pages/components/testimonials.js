6ca7e1a15193c4e3697857d11453d0b8d4b0a028
Dhruv D Jain <dhruv@iamsizzling.com>
2022-06-04 18:01:56 +0530

    added genre images

--- src/pages/components/testimonials.js
+++ src/pages/components/testimonials.js
@@ -6,7 +6,7 @@
       content:
         'It really saves me time and effort. It is exactly what our business has been lacking. EEZY is the most valuable business resource we have EVER purchased. After using EEZY my business skyrocketed!',
       avatar:
-        'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+        'https://ik.imagekit.io/halcyon2022/Icons/Lit_FjxVhtjjw.png',
     },
     {
       name: 'Gaming',
@@ -15,7 +15,7 @@
       content:
         "I didn't even need training. We've used EEZY for the last five years. I have gotten at least 50 times the value from EEZY. I made back the purchase price in just 48 hours!",
       avatar:
-        'https://drive.google.com/uc?export=view&id=1yHmeldI0MOa76oc9GWrYjIHRWXFjmpYb',
+        'https://ik.imagekit.io/halcyon2022/Icons/Gaming_K1DFCy2IU.png',
     },
     {
       name: 'Theater',
@@ -24,7 +24,7 @@
       content:
         "Thank you for making it painless, pleasant and most of all, hassle free! I'm good to go. No matter where you go, EEZY is the coolest, most happening thing around! I love EEZY!",
       avatar:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80',
+        'https://ik.imagekit.io/halcyon2022/Icons/theater_1MSjodu5P.png',
     },
     {
       name: 'Music',
@@ -33,7 +33,7 @@
       content:
         'I am so pleased with this product. EEZY is both attractive and highly adaptable. Without EEZY, we would have gone bankrupt by now. Thank you for creating this product!',
       avatar:
-        'https://images.unsplash.com/photo-1606513542745-97629752a13b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+        'https://ik.imagekit.io/halcyon2022/Icons/Music_Nx5kjpztW.png',
     },
     {
       name: 'Fine Arts',
@@ -42,7 +42,7 @@
       content:
         "Thank you for making it painless, pleasant and most of all, hassle free! I'm good to go. No matter where you go, EEZY is the coolest, most happening thing around! I love EEZY!",
       avatar:
-        'https://ik.imagekit.io/dhruvja/7410215_jMqWVte0x.png',
+        'https://ik.imagekit.io/halcyon2022/Icons/Fine_Arts_LN8zasKo1.png',
     },
     {
       name: 'Dance',
@@ -51,6 +51,6 @@
       content:
         'I am so pleased with this product. EEZY is both attractive and highly adaptable. Without EEZY, we would have gone bankrupt by now. Thank you for creating this product!',
       avatar:
-        'https://drive.google.com/uc?export=view&id=1atMtGRTBXVYe8v_oYTkKRqYs09iRWbsD',
+        'https://ik.imagekit.io/halcyon2022/Icons/dance-1_SQCSdfoe0.png',
     },
   ];
