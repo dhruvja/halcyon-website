5c003efc645ac5f1c13baff5654ffb442e478de4
Dhruv D Jain <dhruv@iamsizzling.com>
2022-06-05 08:16:45 +0530

    changed some data

--- src/pages/components/EventPost.js
+++ src/pages/components/EventPost.js
@@ -97,7 +97,7 @@ function EventPost(props) {
               return value + ' ';
             })}
         </Text>
-        <BlogAuthor name={data.date} date={new Date('2021-04-06T19:01:27Z')} />
+        <BlogAuthor name={data.date} time={data.time} />
         <br />
         <br />
           <Button
@@ -116,15 +116,9 @@ function EventPost(props) {
 const BlogAuthor = props => {
   return (
     <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
-      <Image
-        borderRadius="full"
-        boxSize="40px"
-        src="https://100k-faces.glitch.me/random-image"
-        alt={`Avatar of ${props.name}`}
-      />
       <Text fontWeight="medium">{props.name}</Text>
       <Text>—</Text>
-      <Text>{props.date.toLocaleDateString()}</Text>
+      <Text>{props.time}</Text>
     </HStack>
   );
 };
